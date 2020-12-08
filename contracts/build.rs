368121c1aa795352a0a3e8dd561953d8b413cb16
Felix Leupold <felixleupold90@gmail.com>
2020-12-08 11:54:29 +0100

    Add rinkeby deployment info

--- contracts/build.rs
+++ contracts/build.rs
@@ -1,5 +1,7 @@
-use ethcontract_generate::Builder;
-use std::{env, path::Path};
+use ethcontract_generate::{Address, Builder};
+use maplit::hashmap;
+use std::str::FromStr;
+use std::{collections::HashMap, env, path::Path};
 
 #[path = "src/paths.rs"]
 mod paths;
@@ -14,12 +16,18 @@ fn main() {
     // - https://doc.rust-lang.org/cargo/reference/build-scripts.html#cargorerun-if-changedpath
     println!("cargo:rerun-if-changed=build.rs");
 
-    generate_contract("IERC20");
-    generate_contract("IUniswapV2Router02");
-    generate_contract("GPv2Settlement");
+    generate_contract("IERC20", hashmap! {});
+    generate_contract(
+        "IUniswapV2Router02",
+        hashmap! {4 => Address::from_str("0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D").unwrap()},
+    );
+    generate_contract(
+        "GPv2Settlement",
+        hashmap! {4 => Address::from_str("0x828229A8432A89B8624B6AF91eC0BB65b9517156").unwrap()},
+    );
 }
 
-fn generate_contract(name: &str) {
+fn generate_contract(name: &str, deployments: HashMap<u32, Address>) {
     let artifact = paths::contract_artifacts_dir().join(format!("{}.json", name));
     let dest = env::var("OUT_DIR").unwrap();
 
@@ -29,6 +37,9 @@ fn generate_contract(name: &str) {
         .with_visibility_modifier(Some("pub"))
         .add_event_derive("serde::Deserialize")
         .add_event_derive("serde::Serialize");
+    for (network, address) in deployments.into_iter() {
+        builder = builder.add_deployment(network, address);
+    }
 
     builder
         .generate()
